07ca24e3d36dda291ebb973a67ba42eac4bc7816
YogeshKumar445 <ykyogesh445@gmail.com>
2026-02-05 10:17:03 +0530

    new file added

---
+++ javascript/datatypes.js
@@ -0,0 +1,15 @@
+var number = 10;
+var text = "Hello";
+var isActive = true;
+var fruits = ["Apple", "Banana"];
+var student = { name: "Yogesh Kumar", age: 20 };
+var value = null;
+var x;
+
+console.log("Number:", number);
+console.log("String:", text);
+console.log("Boolean:", isActive);
+console.log("Array:", fruits);
+console.log("Object:", student);
+console.log("Null:", value);
+console.log("Undefined:", x);
